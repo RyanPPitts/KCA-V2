1db0e42247d1eac41a3b70e00425356407f4991e
RyanPPitts <ryanpitts26@gmail.com>
2019-11-01 08:41:10 -0700

    Almost gave up... but I prevailed.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  root 'homepage#index'
+
   get 'account/index'
   get 'account/choreslist'
+
   resources :chores
-  devise_for :admins, path: 'admins', controllers: { sessions: "admins/sessions"}
-  devise_for :users, path: 'users', controllers: { sessions: "users/sessions"}
-  root 'homepage#index'
+  
+  devise_for :admins, path: 'admins'
+  devise_for :users, path: 'users'
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
