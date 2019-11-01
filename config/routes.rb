474e1b63f7cea3eb699c0a6d5d67a7fdf09a4da2
RyanPPitts <ryanpitts26@gmail.com>
2019-11-01 01:31:26 -0700

    creating scaffolding for chores
    Creating scaffolding for chores.  Includes title, amount and completed.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :admins, path: 'admins'
-  devise_for :users, path: 'users'
+  devise_for :admins, path: 'admins', controllers: { sessions: "admins/sessions"}
+  devise_for :users, path: 'users', controllers: { sessions: "users/sessions"}
   root 'homepage#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
